1838a0d544e7595e98d2483421e154d7ce61ecc5
Tarundeep Singh <91110790+urirob@users.noreply.github.com>
2024-07-16 14:35:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
+//socket being a duplex stream, we can both read and write to it
+
 const net=require("net");
 
 const server=net.createServer();
 
-server.on("connection",(socket)=>{  
+//storing sockets objects in the clients array to keep the track of all the connected clients to the server
+const clients=[];
+
+server.on("connection",(socket)=> {  
 
     console.log("A new connection to the server!");
-  
-})
+
+
+    socket.on("data",(data)=>{
+        clients.map((s)=>{
+            s.write(data);
+        });
+    });
+
+
+    clients.push(socket);
+
+
+});
 
 server.listen(3008,"127.0.0.1",()=>{
     console.log("opened server on ",server.address());
