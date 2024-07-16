5900aaf30e64e324fd2d21fe69b74a0784c110b6
Tarundeep Singh <91110790+urirob@users.noreply.github.com>
2024-07-16 14:35:34 +0530

    Create client.js

---
+++ client.js
@@ -0,0 +1,69 @@
+//socket being a duplex stream, we can both read and write to it
+
+
+const net=require("net");
+const readline=require("readline/promises");
+
+
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+});
+
+const clearLine=(dir)=>{
+    //creating promise object to avoid callback hell
+    return new Promise((resolve,reject)=>{
+        process.stdout.clearLine(dir,()=>{
+            resolve();
+        });
+    });
+    
+};
+
+
+//using movecursor function
+const moveCursor=(dx,dy)=>{
+    return new Promise((resolve,reject)=>{
+        process.stdout.moveCursor(dx,dy,()=>{
+            resolve();
+        });
+    });
+
+}
+
+
+const socket=net.createConnection(
+    {host:"127.0.0.1",port:3008},
+    async ()=>{
+        console.log("Connected to the server!");
+
+        const ask=async()=>{
+            const message=await rl.question("Enter a message > ");
+            //move the cursor one line up
+            await moveCursor(0,-1);
+            //clear the current line that the cursor is in
+            await clearLine(0);
+            socket.write(message);
+        };
+
+        ask();
+
+        socket.on("data",async(data)=>{
+            //log an empty line 
+            console.log();
+            //move the cursor one line up
+            await moveCursor(0,-1);
+            //clear that line that cursor just moved into
+            await clearLine(0);
+            console.log(data.toString("utf-8"));
+        
+        
+            ask();
+        });
+    }
+);
+
+
+socket.on("end",()=>{
+    console.log("Connection was ended!");
+});
